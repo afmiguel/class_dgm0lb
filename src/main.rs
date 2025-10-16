98658cef9415250b4725d38ed07f0e7a062211e4
Afonso Miguel <afonso.miguel@pucpr.br>
2025-10-16 17:46:33 -0300

    Update from professor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,20 @@ enum LinkedList{
     Node(i32, Box<LinkedList>),
 }
 
+impl LinkedList{
+    pub fn display_list(&self){
+        match self{
+            LinkedList::Nil => println!("nil"),
+            LinkedList::Node(value, tail ) => {
+                print!("{:?}->", value);
+                tail.display_list();
+            }
+        }
+    }
+}
+
+use LinkedList::{Nil, Node};
 
 fn main() {
-    let lista = LinkedList::Node(10, Box::new(LinkedList::Nil));
+    let lista = Node(10, Box::new(Node(20, Box::new(Nil))));
 }
